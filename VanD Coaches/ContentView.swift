c1baebeaa782d9ecf366c834794e5a73d61f3c4b
Derek Stengel <derek.stengel@gmail.com>
2024-11-08 13:39:05 -0700

    Added project file

---
+++ VanD Coaches/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  VanD Coaches
+//
+//  Created by Derek Stengel on 11/8/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ VanD Coaches/VanD_CoachesApp.swift
@@ -0,0 +1,17 @@
+//
+//  VanD_CoachesApp.swift
+//  VanD Coaches
+//
+//  Created by Derek Stengel on 11/8/24.
+//
+
+import SwiftUI
+
+@main
+struct VanD_CoachesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
